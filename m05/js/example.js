163225d1e3710ae5877011142e482854b58ee0d2
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-02-07 18:53:23 -0800

    Update example.js
    updating code to top page requirements

--- m05/js/example.js
+++ m05/js/example.js
@@ -1,18 +1,28 @@
 
-// Prompt the User for a Number
-let table = prompt("Enter a value for the multiplication table:");
+// Prompt the user for a # from 0 to 10
+let table = prompt("Enter a number from 0 to 10 for the multiplication table:");
 
-// Ensure User enters a number
+// Convert the Input to Integer
 table = parseInt(table, 10);
 
-// Initialize Multiplication Table Header
+//If the input IS NULL or NOT # (NaN) and set it to 0 if true
+if (isNaN(table) || table === null) {
+    table = 0;
+}
+
+// Force 0-10, Else set to 0
+if (table < 0 || table > 10) {
+    table = 0;
+}
+
+// Initialize the * 
 let msg = '<h2>Multiplication Table</h2>';
 
-// Generate the multiplication table
+// Multiplication Table
 for (let i = 1; i <= 10; i++) {
-    // Append Each Row
+    // Append each row of the multiplication table to the message
     msg += table + ' x ' + i + ' = ' + (table * i) + '<br />';
 }
 
-// Display Multiplication table
+// Display the * Table
 document.getElementById('content').innerHTML = msg;
