5795f023e669394f7805c42cf67039ab54ae7dbe
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-02-07 18:58:13 -0800

    Update example.js
    updating javascript to fix 0 error

--- m05/js/example.js
+++ m05/js/example.js
@@ -1,28 +1,22 @@
-
-// Prompt the user for a # from 0 to 10
+// Prompt 0 to 10
 let table = prompt("Enter a number from 0 to 10 for the multiplication table:");
 
-// Convert the Input to Integer
+// Convert to INT
 table = parseInt(table, 10);
 
-//If the input IS NULL or NOT # (NaN) and set it to 0 if true
-if (isNaN(table) || table === null) {
+// Check if the input is not a number (NaN) or null and set it to 0 if true
+if (isNaN(table) || table === null || table < 0 || table > 10) {
     table = 0;
 }
 
-// Force 0-10, Else set to 0
-if (table < 0 || table > 10) {
-    table = 0;
-}
-
-// Initialize the * 
+// Initialize Table
 let msg = '<h2>Multiplication Table</h2>';
 
-// Multiplication Table
+// Generate Table
 for (let i = 1; i <= 10; i++) {
     // Append each row of the multiplication table to the message
     msg += table + ' x ' + i + ' = ' + (table * i) + '<br />';
 }
 
-// Display the * Table
+// Display Table
 document.getElementById('content').innerHTML = msg;
