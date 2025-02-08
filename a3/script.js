618c712661bc280939b3247e4d508d7ea624a459
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-02-08 13:06:11 -0800

    Update script.js
    updating script

--- a3/script.js
+++ a3/script.js
@@ -1,48 +1,42 @@
-// Create Rhombus
-function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-    // Call Functions For Upper And Lower Right Parts Of The Rhombus
-    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-}
+// Create Diamond
+function createDiamond(pHeight, pColorEven, pColorOdd, pSymbol) {
+    let rLine = "";
 
-// Upper Right Rhombus
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-    var rLine = "";
-    // Loop Through Each Line Of The Rhombus
+    // Top Diamond
     for (let i = 0; i < pHeight; i++) {
         rLine += "<p>";
-        // Loop Through Each Position In The Line
+        // Left top part
+        for (let j = pHeight - 1; j > i; j--) {
+            rLine += "<span>&nbsp;&nbsp;</span>";
+        }
         for (let j = 0; j <= i; j++) {
-            // Check If Position Is Even Or Odd And Apply Corresponding Color
-            if (j % 2) {
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
+            rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
+        }
+        // Right top part
+        for (let j = 1; j <= i; j++) {
+            rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
         rLine += "</p>";
     }
-    // Update HTML Content
-    document.getElementById("upRight").innerHTML = rLine;
-}
 
-// Lower Right Rhombus
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-    var rLine = "";
-    // Rhombus Loop
-    for (let i = pHeight; i > 0; i--) {
+    // Bottom diamond
+    for (let i = pHeight - 1; i >= 0; i--) {
         rLine += "<p>";
-        // Rhombus Position Loop
-        for (let j = 0; j < i; j++) {
-            // Check Even or Odd
-            if (j % 2) {
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
+        // Left bottom part
+        for (let j = pHeight - 1; j > i; j--) {
+            rLine += "<span>&nbsp;&nbsp;</span>";
+        }
+        for (let j = 0; j <= i; j++) {
+            rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
+        }
+        // Right bottom part
+        for (let j = 1; j <= i; j++) {
+            rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
         rLine += "</p>";
     }
+
     // Update HTML
-    document.getElementById("downRight").innerHTML = rLine;
+    document.getElementById("upRight").innerHTML = rLine;
+    document.getElementById("downRight").innerHTML = "";
 }
