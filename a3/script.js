cddbcaf1fb5e2a54393b58f861b27a8d079976ed
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-02-08 13:12:17 -0800

    Update script.js
    updating JS to for rubric

--- a3/script.js
+++ a3/script.js
@@ -5,38 +5,38 @@ function createDiamond(pHeight, pColorEven, pColorOdd, pSymbol) {
     // Top Diamond
     for (let i = 0; i < pHeight; i++) {
         rLine += "<p>";
-        // Left top part
+        // Left Top
         for (let j = pHeight - 1; j > i; j--) {
             rLine += "<span>&nbsp;&nbsp;</span>";
         }
         for (let j = 0; j <= i; j++) {
             rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
-        // Right top part
+        // Right Top
         for (let j = 1; j <= i; j++) {
             rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
         rLine += "</p>";
     }
 
-    // Bottom diamond
+    // Bottom Diamond
     for (let i = pHeight - 1; i >= 0; i--) {
         rLine += "<p>";
-        // Left bottom part
+        // Left Bottom
         for (let j = pHeight - 1; j > i; j--) {
             rLine += "<span>&nbsp;&nbsp;</span>";
         }
         for (let j = 0; j <= i; j++) {
             rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
-        // Right bottom part
+        // Right Bottom
         for (let j = 1; j <= i; j++) {
             rLine += `<span style='color:${j % 2 ? pColorEven : pColorOdd};'>${pSymbol}</span>`;
         }
         rLine += "</p>";
     }
 
-    // Update HTML
+    // Update Content
     document.getElementById("upRight").innerHTML = rLine;
     document.getElementById("downRight").innerHTML = "";
 }
