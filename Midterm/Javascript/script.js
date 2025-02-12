be6e0253301681e86ec7e2c162e01f4e86596a0c
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-02-11 19:40:18 -0800

    Update script.js
    adding javascript content.

--- Midterm/Javascript/script.js
+++ Midterm/Javascript/script.js
@@ -1,1 +1,6 @@
 
+// Submit Form
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default Entry
+    window.location.href = 'thankyou.html'; // Navigate Thank You Screen
+});
