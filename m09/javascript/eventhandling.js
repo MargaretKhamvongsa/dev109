cc8c6307ef858e430757fea4f73d3bbf30a35914
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-03-04 17:43:53 -0800

    Update eventhandling.js
    adding in comments

--- m09/javascript/eventhandling.js
+++ m09/javascript/eventhandling.js
@@ -1,23 +1,41 @@
+// Clear button
 document.getElementById('clear-button').addEventListener('click', function(event) {
+    // Get dots
     var elements = document.getElementsByClassName('dot');
+    
+    // Remove all Dots
     while (elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
     }
 
+    // Stop propagation
     event.stopPropagation();
 });
 
+// Document Click
 addEventListener('click', function(event) {
-    var dot = document.createElement('div');
-    dot.className = 'dot';
-    dot.style.left = (event.pageX - (dot.offsetWidth / 2)) + 'px';
-    dot.style.top = (event.pageY - (dot.offsetHeight / 2)) + 'px';
+    // Check target
+    if (event.target.id !== 'clear-button' && event.target.id !== 'dot-settings' && event.target.tagName !== 'INPUT' && event.target.tagName !== 'LABEL') {
+        // Create dot
+        var dot = document.createElement('div');
+        dot.className = 'dot';
 
-    var dotSize = document.getElementById('dot-size').value;
-    var dotColor = document.getElementById('dot-color').value;
-    dot.style.height = dotSize + 'px';
-    dot.style.width = dotSize + 'px';
-    dot.style.backgroundColor = dotColor;
+        // Get Dot Size
+        var dotSize = document.getElementById('dot-size').value;
+        // Get color
+        var dotColor = document.getElementById('dot-color').value;
 
-    document.body.appendChild(dot);
+        // Set Dot Size
+        dot.style.height = dotSize + 'px';
+        dot.style.width = dotSize + 'px';
+        // Set color
+        dot.style.backgroundColor = dotColor;
+
+        // Position dot
+        dot.style.left = (event.pageX - (dotSize / 2)) + 'px';
+        dot.style.top = (event.pageY - (dotSize / 2)) + 'px';
+
+        // Append dot
+        document.body.appendChild(dot);
+    }
 });
