3fbae6899736897fbeeb6bc773517789c990dcd2
Margaret Khamvongsa <margaret.khamvongsa@bellevuecollege.edu>
2025-03-13 19:26:34 -0700

    Update script.js
    adding code for wildcard screen

--- finalp1/Javascript/script.js
+++ finalp1/Javascript/script.js
@@ -4,3 +4,19 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault(); // Prevent default Entry
     window.location.href = 'thankyou.html'; // Navigate Thank You Screen
 });
+
+// JavaScript for Wild card page
+document.addEventListener('DOMContentLoaded', () => {
+    const audio = document.querySelector('audio');
+
+    // Log a message when the audio is played
+    audio.addEventListener('play', () => {
+        console.log('Music is now playing!');
+    });
+
+    // Show an alert when the video ends
+    const video = document.querySelector('video');
+    video.addEventListener('ended', () => {
+        alert('Hope you enjoyed the video about space!');
+    });
+});
